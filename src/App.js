f772364e1859d1f7517b2ba21605438b43b76c61
Matthew Linsley <phoenixstar117@gmail.com>
2023-05-15 16:04:44 -0700

    configured for blank template to begin build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import BlogLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello Portfolio Website
-    </div>
-  );
+	return (
+		<div className="App">
+		<Navbar dark color='primary' sticky='top' expand='md'>
+				<Container>
+						<NavbarBrand href='/'>
+							<img src={BlogLogo} alt='blog logo' />
+						</NavbarBrand>
+					</Container>
+				</Navbar>
+				I'm ready to build!
+		</div>
+	);
 }
 
 export default App;
